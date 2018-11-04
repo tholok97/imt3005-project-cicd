fc866fe9ba0877f43195e21faa49db9a19b28a86
tholok97 <thomahl@stud.ntnu.no>
2018-11-04 11:03:47 +0000

    cleaned up compile script, fixed latex errors and added a few refs

--- report/c.sh
+++ report/c.sh
@@ -1,5 +1,5 @@
 # compiles report into a pdf
-pdflatex --shell-escape template.tex
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
 bibtex template.aux
-pdflatex --shell-escape template.tex
-pdflatex --shell-escape template.tex
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
