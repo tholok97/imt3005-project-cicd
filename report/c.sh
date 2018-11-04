abb3b9bcad7451a3e22f35f5122e0a3f8b1df643
tholok97 <thomahl@stud.ntnu.no>
2018-11-04 14:40:32 +0000

    changed report name from template to report

--- report/c.sh
+++ report/c.sh
@@ -1,5 +1,5 @@
 # compiles report into a pdf
-pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
-bibtex template.aux
-pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
-pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error template.tex
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error report.tex
+bibtex report.aux
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error report.tex
+pdflatex --shell-escape --interaction nonstopmode -halt-on-error -file-line-error report.tex
